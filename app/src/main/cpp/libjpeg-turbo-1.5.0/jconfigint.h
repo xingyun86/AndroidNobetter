89207d1b39129e3fb74b51b939cc8836fe5d776e
ppshuai <gz_ppshuai@yeah.net>
2017-01-23 12:45:41 +0800

    AndroidStudio CMake 编译JNI库

---
+++ app/src/main/cpp/libjpeg-turbo-1.5.0/jconfigint.h
@@ -0,0 +1,22 @@
+/* libjpeg-turbo build number */
+#undef BUILD
+#define BUILD "20160906"
+
+/* How to obtain function inlining. */
+#undef INLINE
+#define INLINE __inline
+
+/* Define to the full name of this package. */
+#undef PACKAGE_NAME
+#define PACKAGE_NAME "turbojpeg"
+
+/* Version number of package */
+#undef VERSION
+#define VERSION "1.50"
+
+/* The size of `size_t', as computed by sizeof. */
+#undef SIZEOF_SIZE_T
+#define SIZEOF_SIZE_T size_t
+
+/* Support the Independent JPEG Group's software */
+#define PPM_SUPPORTED
